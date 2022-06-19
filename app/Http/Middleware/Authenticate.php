855a2a5c6f238fe2d10740e2caa5a5bc4bfdcaad
Muhep Atasoy <muhepatasoy06@gmail.com>
2022-06-19 17:18:36 +0300

    (Test) Allow per page visibility for guest

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -2,16 +2,35 @@
 
 namespace BookStack\Http\Middleware;
 
+use BookStack\Entities\Repos\PageRepo;
 use Closure;
 use Illuminate\Http\Request;
 
 class Authenticate
 {
+    private PageRepo $pageRepo;
+
+    public function __construct(PageRepo $pageRepo)
+    {
+        $this->pageRepo = $pageRepo;
+    }
+
     /**
      * Handle an incoming request.
      */
     public function handle(Request $request, Closure $next)
     {
+        $route = $request->route();
+        if ($route->hasParameter('pageSlug')) {
+            $bookSlug = $route->parameter('bookSlug');
+            $pageSlug = $route->parameter('pageSlug');
+            $page = $this->pageRepo->getByOldSlug($bookSlug, $pageSlug);
+
+            if (userCan('page-view', $page)) {
+                return $next($request);
+            }
+        }
+
         if (!hasAppAccess()) {
             if ($request->ajax()) {
                 return response('Unauthorized.', 401);
